b44270232ad162d5b4a3241805098d29147728bb
Salix8 <saulpachecotrilles@gmail.com>
2021-11-16 13:42:04 +0100

    tema 2 actualizado

---
+++ tema2/1IntroAjax/1mostrarDatos/js/ejercicio1.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+
+
+    var XMLHttpRequestObject = false;
+
+    if (window.XMLHttpRequest) {
+        XMLHttpRequestObject = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    
+}
+
+function sacarDatos(datos, idDiv){
+    if(XMLHttpRequestObject) {
+        var objeto = document.getElementById(idDiv);
+        XMLHttpRequestObject.open("GET", datos);
+        XMLHttpRequestObject.onreadystatechange = function(){
+            if (XMLHttpRequestObject.readyState == 4 &&                   
+                XMLHttpRequestObject.status == 200) {
+                objeto.innerHTML = XMLHttpRequestObject.responseText;
+            }
+        }
+        XMLHttpRequestObject.send(null);
+    } 
+}
