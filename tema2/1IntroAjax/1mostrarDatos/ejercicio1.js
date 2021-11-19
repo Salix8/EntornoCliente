3774975c2383440fac71387d3682f8de4f103aad
Salix8 <saulpachecotrilles@gmail.com>
2021-11-19 13:57:53 +0100

    tema2

--- tema2/1IntroAjax/1mostrarDatos/ejercicio1.js
+++ tema2/1IntroAjax/1mostrarDatos/ejercicio1.js
@@ -1,26 +1,25 @@
 window.addEventListener(`load`, ()=>{
 
-    document.getElementById(`contenedor`).addEventListener(`click`, ()=> {
-        sacarDatos(`datos.txt`);
-    });
-
-    var XMLHttpRequestObject = false;
-
+    let XMLHttpRequestObject = false;
+    
     if (window.XMLHttpRequest) {
         XMLHttpRequestObject = new XMLHttpRequest();
     } else if (window.ActiveXObject) {
         XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
     }
+
+    document.getElementById(`btnMostrar`).addEventListener(`click`, ()=> {
+        sacarDatos(`datos.txt`, XMLHttpRequestObject);
+    });    
 });
 
 
-function sacarDatos(datos){
+function sacarDatos(datos, XMLHttpRequestObject){
     if(XMLHttpRequestObject) {
         var objeto = document.getElementById(`contenedor`);
         XMLHttpRequestObject.open("GET", datos);
         XMLHttpRequestObject.onreadystatechange = function(){
-            if (XMLHttpRequestObject.readyState == 4 &&                   
-                XMLHttpRequestObject.status == 200) {
+            if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200) {
                 objeto.innerHTML = XMLHttpRequestObject.responseText;
             }
         }
---
+++ tema2/1IntroAjax/2PaginasWeb/ejercicio2.js
@@ -0,0 +1,28 @@
+window.addEventListener(`load`, ()=>{
+
+    let XMLHttpRequestObject = false;
+    
+    if (window.XMLHttpRequest) {
+        XMLHttpRequestObject = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    document.getElementById(`inicio`).addEventListener(`click`, ()=> {
+        sacarDatos(`inicio.html`, XMLHttpRequestObject);
+    });    
+});
+
+
+function sacarDatos(datos, XMLHttpRequestObject){
+    if(XMLHttpRequestObject) {
+        var objeto = document.getElementById(`contenedor`);
+        XMLHttpRequestObject.open("GET", datos);
+        XMLHttpRequestObject.onreadystatechange = function(){
+            if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200) {
+                objeto.innerHTML = XMLHttpRequestObject.responseText;
+            }
+        }
+    } 
+    XMLHttpRequestObject.send(null);
+}
