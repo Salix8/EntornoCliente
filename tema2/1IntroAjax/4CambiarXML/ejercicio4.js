dd47a7c2685045c9583e8012bbce757d7d735105
Salix8 <saulpachecotrilles@gmail.com>
2021-11-24 13:54:05 +0100

    acabado hasta el 6 y el 9

--- tema2/1IntroAjax/4CambiarXML/ejercicio4.js
+++ tema2/1IntroAjax/4CambiarXML/ejercicio4.js
@@ -1,29 +1,39 @@
-window.addEventListener(`load`, ()=>{
 
-    let XMLHttpRequestObject = false;
-    
-    if (window.XMLHttpRequest) {
-        XMLHttpRequestObject = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
-    }
 
-    document.getElementById(`btnMostrar`).addEventListener(`click`, ()=> {
-        sacarDatos(`archivo.xml`, XMLHttpRequestObject);
-    });
+window.addEventListener(`click`, ()=>{
 
+  descargaArchivo();
 });
 
+function descargaArchivo() {
+  let peticionHttp = new XMLHttpRequest();
+  // Preparar la funcion de respuesta
+  peticionHttp.onreadystatechange = muestraContenido;
+  // Realizar peticion HTTP
+  peticionHttp.open('GET', 'libros.xml', true);
+  peticionHttp.send(null);
 
-function sacarDatos(datos, XMLHttpRequestObject){
-    if(XMLHttpRequestObject) {
-        var objeto = document.getElementById(`contenedor`);
-        XMLHttpRequestObject.open("GET", datos);
-        XMLHttpRequestObject.onreadystatechange = function(){
-            if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200) {
-                objeto.innerHTML = XMLHttpRequestObject.responseText;
-            }
+  function muestraContenido() {
+    if(peticionHttp.readyState == 4) {
+      if(peticionHttp.status == 200) {
+        //Creamos el objeto de tipo documento XML
+        var documentoXml = peticionHttp.responseXML;
+        //Obtenemos la raíz del documento
+        var root = documentoXml.getElementsByTagName("Obras_literarias")[0];
+        var tope = root.getElementsByTagName("Libro").length;
+        //Recorremos todos los elementos Libro del documento
+        for(var i = 0; i < tope; i++){ 
+          libro = root.getElementsByTagName("Libro")[i]; 
+          autor = libro.getElementsByTagName("Autor")[0].firstChild.nodeValue;
+          titulo = libro.getElementsByTagName("Titulo")[0].firstChild.nodeValue;
+          muestraHTML('contenedor', "Autor: " + autor + ", Titulo "+titulo+"<br/>");
         }
-    } 
-    XMLHttpRequestObject.send(null);
+      }
+    }
+  }
+  function muestraHTML(id, texto){
+    if(document.getElementById){
+      document.getElementById(id).innerHTML += texto;
+    }
+  }
 }
--- tema2/1IntroAjax/9Gif/script.php
+++ tema2/1IntroAjax/9Gif/script.php
@@ -1,5 +1,5 @@
 <?php
-
+    sleep(2);
     echo "Hola<br>";
     // Obteniendo la fecha actual con hora, minutos y segundos en PHP
     echo date('d-m-Y H:i:s');
