fc55c493b0bec2b240b32c09fa7272733ce59774
Salix8 <saulpachecotrilles@gmail.com>
2021-12-17 13:52:16 +0100

    inicio con JQuery

---
+++ tema3/1JQueryBasico/1Creacion de capas/ejercicio1.js
@@ -0,0 +1,5 @@
+$(document).ready(()=>{
+    var divs = $("div");
+    divs.css("color", "#008f39");
+    alert(`${divs.length}`);
+})
---
+++ tema3/1JQueryBasico/3cambiarColortxt/2MostrarOcultar/ejercicio2.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $(`#btn1`).click(()=>{
+        $(`#img1ej2`).show(2500);
+    })
+    $(`#btn2`).click(()=>{
+        $(`#img1ej2`).hide(`slow`);
+    })
+})
